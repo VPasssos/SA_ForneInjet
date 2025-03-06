1839fbdd3a4719fe663aafa667eb36f02317229b
isabellasz09 <isabellacarolinadesouza17@gmail.com>
2025-03-06 18:42:29 -0300

    Correção de código

--- CRUD_Fornecedores.py
+++ CRUD_Fornecedores.py
@@ -1,36 +1,38 @@
-def create_user(nome, cnpj,email,endereco, telefone,cont_principal,site):
+from Config import get_connection
+
+def create_fornecedor(nome_fornecedor, cnpj, email, endereco, telefone , contato_principal, website):
     conn = get_connection()
     cursor = conn.cursor()
-    query = "insert usuario(nome, telefone, email, usuario, senha) VALUES(%s,%s,%s,%s,%s)"
-    cursor.execute(query,(nome,cnpj,email, endereco, telefone, cont_principal, site))
+    query = "insert Fornecedor (nome_fornecedor, cnpj, email, endereco, telefone, contato_principal, website) VALUES(%s,%s,%s,%s,%s,%s,%s)"
+    cursor.execute(query,(nome_fornecedor, cnpj, email, endereco, telefone, contato_principal, website))
     conn.commit()
     cursor.close()
     conn.close()
 
-def read_users():
+def read_fornecedor():
     conn = get_connection()
     cursor = conn.cursor()
-    query = "SELECT * FROM usuario"
+    query = "SELECT * FROM Fornecedor"
     cursor.execute(query)
     result = cursor.fetchall()
     cursor.close()
     conn.close()
     return result
 
-def update_user(user_id,nome,cnpj,email,endereco,telefone,cont_principal,site):
+def update_fornecedor(idFornecedor, nome_fornecedor, cnpj, email, endereco, telefone, contato_principal, website):
     conn = get_connection()
     cursor = conn.cursor()
-    query = "UPDATE aluno SET nome=%s, telefone=%s, email=%s, usuario=%s, senha=%s WHERE idusuario=%s"
-    cursor.execute(query,(user_id, nome, cnpj, email, endereco, telefone, cont_principal, site))
+    query = "UPDATE Fornecedor SET nome=%s, cnpj=%s, email=%s, endereco=%s, telefone=%s, contato_principal=%s, website=%s WHERE idFornecedor=%s"
+    cursor.execute(query,(idFornecedor, nome_fornecedor, cnpj, email, endereco, telefone, contato_principal, website))
     conn.commit()
     cursor.close()
     conn.close()
 
-def delete_user(user_id):
+def delete_fornecedor(idFornecedor):
     conn = get_connection()
     cursor = conn.cursor()
-    query = "DELETE FROM aluno WHERE idusuario = %s"
-    cursor.execute(query,(user_id))
+    query = "DELETE FROM Fornecedor WHERE idFornecedor = %s"
+    cursor.execute(query,(idFornecedor,))
     conn.commit()
     cursor.close()
     conn.close()
