411f22fb74d3de476a8fe86a55cc83d43a6d453c
isabellasz09 <isabellacarolinadesouza17@gmail.com>
2025-03-06 19:17:20 -0300

    Correção de código

--- CRUD_Produtos.py
+++ CRUD_Produtos.py
@@ -1,12 +1,12 @@
 from Config import get_connection
 
-def create_produto(tipo, marca, modelo, capacidade_de_injecao, força_de_fechamento, tipo_de_controle, preço_médio_dolar,preço_médio_real, 
-Fornecedor, observaçoes):
+def create_produto(tipo, marca, modelo, capacidade_de_injeçao, força_de_fechamento, tipo_de_controle, preço_medio_USD,preço_medio_BRL, 
+fornecedor, observacao):
 
     conn = get_connection()
     cursor = conn.cursor()
-    query = "insert produto(tipo, marca, modelo, capacidade_de_injecao,força_de_fechamento, tipo_de_controle, preço_médio_dolar,preço_médio_real, Fornecedor, observaçoes) VALUES( %s, %s, %s, %s, %s, %s ,%s, %s, %s,%s)"
-    cursor.execute(query, (tipo, marca, modelo, capacidade_de_injecao, força_de_fechamento, tipo_de_controle, preço_médio_dolar,preço_médio_real, Fornecedor, observaçoes))
+    query = "insert produto(tipo, marca, modelo, capacidade_de_injecao,força_de_fechamento, tipo_de_controle, preço_medio_USD,preço_medio_BRL, fornecedor, observacao) VALUES( %s, %s, %s, %s, %s, %s ,%s, %s, %s,%s)"
+    cursor.execute(query, (tipo, marca, modelo, capacidade_de_injeçao, força_de_fechamento, tipo_de_controle, preço_medio_USD,preço_medio_BRL, fornecedor, observacao))
     conn.commit()
     cursor.close()
     conn.close()
@@ -22,22 +22,22 @@ def read_produto():
     conn.close()
     return result
 
-def update_produto(tipo, marca, modelo, capacidade_de_injecao, força_de_fechamento, tipo_de_controle, preço_médio_dolar,preço_médio_real, Fornecedor, observaçoes , produtoID):
+def update_produto(tipo, marca, modelo, capacidade_de_injeçao, força_de_fechamento, tipo_de_controle, preço_medio_USD,preço_medio_BRL, fornecedor, observacao, idMaquinas):
 
     conn = get_connection()
     cursor = conn.cursor()
     query = "UPDATE produto SET tipo = %s, marca = %s, modelo = %s, capacidade_de_injecao = %s , força_de_fechamento = %s, tipo_de_controle = %s, observaçoes = %s WHERE produtoID = %s"
-    cursor.execute(query, (tipo, marca, modelo, capacidade_de_injecao, força_de_fechamento, tipo_de_controle, preço_médio_dolar,preço_médio_real, Fornecedor, observaçoes , produtoID))
+    cursor.execute(query, (tipo, marca, modelo, capacidade_de_injeçao, força_de_fechamento, tipo_de_controle, preço_medio_USD,preço_medio_BRL, fornecedor, observacao, idMaquinas))
     conn.commit()
     cursor.close()
     conn.close()
 
-def delete_produto(produtoID):
+def delete_produto(idMaquinas):
 
     conn = get_connection()
     cursor = conn.cursor()
     query = "DELETE FROM produto WHERE produtoID = %s"
-    cursor.execute(query, (produtoID,))
+    cursor.execute(query, (idMaquinas,))
     conn.commit()
     cursor.close()
     conn.close()
