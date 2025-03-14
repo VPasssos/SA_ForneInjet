b4441b338a7308e56db0b671f1d4b7ae79f593ee
Vinicius dos Passos da Silva <viniciuspassos030@gmail.com>
2025-03-13 22:10:36 -0300

    Modificação de Codigo

--- Tela_ADM.py
+++ Tela_ADM.py
@@ -1,6 +1,8 @@
 import tkinter as tk
 from tkinter import ttk
-from CRUD_Funcionarios import create_funcionario, update_funcionario, delete_funcionario
+from CRUD_Produtos import create_produto, update_produto, delete_produto
+from CRUD_Fornecedores import create_fornecedor, update_fornecedor, delete_fornecedor
+from CRUD_Funcionarios import create_funcionario, update_funcionario, delete_funcionario, read_funcionarios
 
 class TelaAbas_ADM:
     def __init__(self, root):
@@ -16,105 +18,115 @@ class TelaAbas_ADM:
         self.notebook.add(self.inicio_frame, text="Inicio")
         self.create_inicio_widgets()
 
+        # Aba produtos
+        self.produto_frame = ttk.Frame(self.notebook)
+        self.notebook.add(self.produto_frame, text="Produto")
+        self.create_produto_widgets()
+        
+        # Aba fornecedor
+        self.fornecedor_frame = ttk.Frame(self.notebook)
+        self.notebook.add(self.fornecedor_frame, text="Fornecedor")
+        self.create_fornecedor_widgets()
+
         # Aba Funcionario
         self.funcionario_frame = ttk.Frame(self.notebook)
         self.notebook.add(self.funcionario_frame, text="Funcionario")
         self.create_funcionario_widgets()
 
-    # Métodos para a aba Funcionario
+    # Métodos para a aba inicial
     def create_inicio_widgets(self):
-        print("Teste")
-    
+        print("Tela Inicial")
+
+    # Métodos para a aba produto
+    def create_produto_widgets(self):
+        self.create_crud_widgets(self.produto_frame, self.criar_produto, self.listar_produtos, self.atualizar_produto, self.deletetar_produto)
+
+    def criar_produto(self):
+        create_produto(self)
+
+    def listar_produtos(self):
+        read_produtos(self)
+
+    def atualizar_produto(self):
+        update_produto(self)
+
+    def deletetar_produto(self):
+        delete_produto(self)
+
+    # Métodos para a aba fornecedor
+    def create_fornecedor_widgets(self):
+        self.create_crud_widgets(self.fornecedor_frame, self.criar_fornecedor, self.listar_fornecedores, self.atualizar_fornecedor, self.deletetar_fornecedor)
+
+    def criar_fornecedor(self):
+        create_fornecedor(self)
+
+    def listar_fornecedores(self):
+        read_fornecedores(self)
+
+    def atualizar_fornecedor(self):
+        update_fornecedor(self)
+
+    def deletetar_fornecedor(self):
+        delete_fornecedor(self)
+
+    # Métodos para a aba funcionario
     def create_funcionario_widgets(self):
-        # Labels
-        tk.Label(self.funcionario_frame, text="Nome: ").grid(row=0, column=0)
-        tk.Label(self.funcionario_frame, text="Email: ").grid(row=1, column=0)
-        tk.Label(self.funcionario_frame, text="Telefone: ").grid(row=2, column=0)
-        tk.Label(self.funcionario_frame, text="Cargo: ").grid(row=3, column=0)
-        tk.Label(self.funcionario_frame, text="Data Admissão: ").grid(row=4, column=0)
-        tk.Label(self.funcionario_frame, text="Situação: ").grid(row=5, column=0)
-        tk.Label(self.funcionario_frame, text="Permissão: ").grid(row=6, column=0)
-        tk.Label(self.funcionario_frame, text="Usuario: ").grid(row=7, column=0)
-        tk.Label(self.funcionario_frame, text="Senha: ").grid(row=8, column=0)
-        tk.Label(self.funcionario_frame, text="ID (para atualizar/excluir): ").grid(row=9, column=0)
+        self.create_crud_widgets(self.funcionario_frame, self.criar_funcionario, self.listar_funcionarios, self.atualizar_funcionario, self.deletetar_funcionario)
 
-        # Entradas
-        self.func_nome_funcionario_entry = tk.Entry(self.funcionario_frame)
-        self.func_email_entry = tk.Entry(self.funcionario_frame)
-        self.func_telefone_entry = tk.Entry(self.funcionario_frame)
-        self.func_cargo_entry = tk.Entry(self.funcionario_frame)
-        self.func_data_admissao_entry = tk.Entry(self.funcionario_frame)
-        self.func_situacao_entry = tk.Entry(self.funcionario_frame)
-        self.func_permissao_entry = tk.Entry(self.funcionario_frame)
-        self.func_usuario_entry = tk.Entry(self.funcionario_frame)
-        self.func_senha_entry = tk.Entry(self.funcionario_frame, show="*")
-        self.func_id_entry = tk.Entry(self.funcionario_frame)
-
-        # Posicionamento
-        self.func_nome_funcionario_entry.grid(row=0, column=1)
-        self.func_email_entry.grid(row=1, column=1)
-        self.func_telefone_entry.grid(row=2, column=1)
-        self.func_cargo_entry.grid(row=3, column=1)
-        self.func_data_admissao_entry.grid(row=4, column=1)
-        self.func_situacao_entry.grid(row=5, column=1)
-        self.func_permissao_entry.grid(row=6, column=1)
-        self.func_usuario_entry.grid(row=7, column=1)
-        self.func_senha_entry.grid(row=8, column=1)
-        self.func_id_entry.grid(row=9, column=1)
-
-        # Botões
-        tk.Button(self.funcionario_frame, text="Criar Funcionario", command=self.criar_funcionarios).grid(row=10, column=0)
-        tk.Button(self.funcionario_frame, text="Listar Funcionarios", command=self.listar_funcionarios).grid(row=10, column=1)
-        tk.Button(self.funcionario_frame, text="Atualizar Funcionario", command=self.atualizar_funcionario).grid(row=11, column=0)
-        tk.Button(self.funcionario_frame, text="Excluir Funcionario", command=self.deletetar_funcionario).grid(row=11, column=1)
-
-        # Tabela para exibir dados
-        self.func_table = ttk.Treeview(self.funcionario_frame, columns=("ID", "Nome", "Email", "Telefone", "Cargo", "Data Admissao", "Situacao", "Permissao", "Usuario", "Senha"), show="headings")
-        self.func_table.grid(row=12, column=0, columnspan=2)
-        
-        # Definindo as colunas
-        for col in self.func_table["columns"]:
-            self.func_table.heading(col, text=col)
-        
-        # Adicionar uma barra de rolagem
-        self.scrollbar = ttk.Scrollbar(self.funcionario_frame, orient="vertical", command=self.func_table.yview)
-        self.scrollbar.grid(row=12, column=2, sticky="ns")
-        self.func_table.configure(yscrollcommand=self.scrollbar.set)
-
-        # Vincular o evento de clique na linha
-        self.func_table.bind("<ButtonRelease-1>", self.row_selected_funcionario)
-
-    def row_selected_funcionario(self, event):
-        selected_item = self.func_table.selection()[0]
-        values = self.func_table.item(selected_item, "values")
-        
-        # Preencher os campos com os dados da linha
-        self.func_nome_funcionario_entry.delete(0, tk.END)
-        self.func_nome_funcionario_entry.insert(0, values[1])  # Nome
-        self.func_email_entry.delete(0, tk.END)
-        self.func_email_entry.insert(0, values[2])  # Email
-        self.func_telefone_entry.delete(0, tk.END)
-        self.func_telefone_entry.insert(0, values[3])  # Telefone
-        self.func_cargo_entry.delete(0, tk.END)
-        self.func_cargo_entry.insert(0, values[4])  # Cargo
-        self.func_data_admissao_entry.delete(0, tk.END)
-        self.func_data_admissao_entry.insert(0, values[5])  # Data Admissão
-        self.func_situacao_entry.delete(0, tk.END)
-        self.func_situacao_entry.insert(0, values[6])  # Situação
-        self.func_permissao_entry.delete(0, tk.END)
-        self.func_permissao_entry.insert(0, values[7])  # Permissão
-        self.func_usuario_entry.delete(0, tk.END)
-        self.func_usuario_entry.insert(0, values[8])  # Usuario
-        self.func_senha_entry.delete(0, tk.END)
-        self.func_senha_entry.insert(0, values[9])  # Senha
-        self.func_id_entry.delete(0, tk.END)
-        self.func_id_entry.insert(0, values[0])  # ID
-
-    def criar_funcionarios(self):
+    def criar_funcionario(self):
         create_funcionario(self)
+
     def listar_funcionarios(self):
-        read_funcionario(self)
+        read_funcionarios(self)
+
     def atualizar_funcionario(self):
         update_funcionario(self)
+
     def deletetar_funcionario(self):
         delete_funcionario(self)
+
+    # Método para criar os widgets CRUD em qualquer aba
+    def create_crud_widgets(self, frame, criar_command, listar_command, atualizar_command, deletar_command):
+        # Labels
+        labels = ["Nome", "Email", "Telefone", "Cargo", "Data Admissão", "Situação", "Permissão", "Usuario", "Senha", "ID (para atualizar/excluir)"]
+        for i, label in enumerate(labels):
+            tk.Label(frame, text=f"{label}: ").grid(row=i, column=0)
+
+        # Entradas
+        self.entries = {}
+        for i, label in enumerate(labels):
+            self.entries[label] = tk.Entry(frame)
+            self.entries[label].grid(row=i, column=1)
+
+        # Botões
+        tk.Button(frame, text="Criar", command=criar_command).grid(row=10, column=0)
+        tk.Button(frame, text="Listar", command=listar_command).grid(row=10, column=1)
+        tk.Button(frame, text="Atualizar", command=atualizar_command).grid(row=11, column=0)
+        tk.Button(frame, text="Excluir", command=deletar_command).grid(row=11, column=1)
+
+        # Tabela para exibir dados
+        self.table = ttk.Treeview(frame, columns=("ID", "Nome", "Email", "Telefone", "Cargo", "Data Admissao", "Situacao", "Permissao", "Usuario", "Senha"), show="headings")
+        self.table.grid(row=12, column=0, columnspan=2)
+        
+        # Definindo as colunas
+        for col in self.table["columns"]:
+            self.table.heading(col, text=col)
+        
+        # Adicionar uma barra de rolagem
+        scrollbar = ttk.Scrollbar(frame, orient="vertical", command=self.table.yview)
+        scrollbar.grid(row=12, column=2, sticky="ns")
+        self.table.configure(yscrollcommand=scrollbar.set)
+
+        # Vincular o evento de clique na linha
+        self.table.bind("<ButtonRelease-1>", self.row_selected)
+
+    def row_selected(self, event):
+        selected_item = self.table.selection()[0]
+        values = self.table.item(selected_item, "values")
+        
+        # Preencher os campos com os dados da linha
+        labels = ["Nome", "Email", "Telefone", "Cargo", "Data Admissao", "Situacao", "Permissao", "Usuario", "Senha", "ID"]
+        for i, label in enumerate(labels):
+            entry = self.entries[label]
+            entry.delete(0, tk.END)
+            entry.insert(0, values[i])  # Preenche cada entrada com o valor correspondente
