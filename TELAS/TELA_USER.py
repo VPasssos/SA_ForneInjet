d5ba0fc24e40fff41d524d6efff0b00a6416e592
isabellasz09 <isabellacarolinadesouza17@gmail.com>
2025-05-13 22:09:34 -0300

    Correções

--- TELAS/TELA_USER.py
+++
@@ -1,5 +0,0 @@
-class TELA_USER:
-    def __init__(self, root):
-        self.root = root
-        self.root.title("Sistema ForneInjet - Gestão Completa - USER")
-        self.root.geometry("1200x800")
